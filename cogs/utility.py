d558724cbbb48dc53fe76894025b4cb7172580fc
PallavBS <32361541+PallavBS@users.noreply.github.com>
2017-11-25 00:47:02 -0800

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -924,12 +924,12 @@ class Utility:
         '''Auto Update command, checks if you have latest version
         Use tags github-token to find out how to set up this token'''
         git = self.bot.get_cog('Git')
-        if not await git.starred('kyb3r/selfbot.py'): return await ctx.send('**This command is disabled as the user have not starred <https://github.com/kyb3r/selfbot.py>**')
+        if not await git.starred('PallavBS/Discord-SelfBot-1'): return await ctx.send('**This command is disabled as the user have not starred <https://github.com/PallavBS/Discord-Selfbot-1>**')
         # get username
         username = await git.githubusername()
-        async with self.session.get('https://api.github.com/repos/kyb3r/selfbot.py/git/refs/heads/rewrite', headers={"Authorization": f"Bearer {git.githubtoken}"}) as resp:
+        async with self.session.get('https://api.github.com/repos/PallavBS/Discord-Selfbot-1/git/refs/heads/rewrite', headers={"Authorization": f"Bearer {git.githubtoken}"}) as resp:
             if 300 > resp.status >= 200:
-                async with self.session.post(f'https://api.github.com/repos/{username}/selfbot.py/merges', json={"head": (await resp.json())['object']['sha'], "base": "rewrite", "commit_message": "Updating Bot"}, headers={"Authorization": f"Bearer {git.githubtoken}"}) as resp2:
+                async with self.session.post(f'https://api.github.com/repos/{username}/Discord-Selfbot-1/merges', json={"head": (await resp.json())['object']['sha'], "base": "rewrite", "commit_message": "Updating Bot"}, headers={"Authorization": f"Bearer {git.githubtoken}"}) as resp2:
                     if 300 > resp2.status >= 200:
                         if resp2.status == 204:
                             return await ctx.send('Already at latest version!')
@@ -937,9 +937,9 @@ class Utility:
                     else:
                         if resp2.status == 409:
                             return await ctx.send('Merge conflict, you did some commits that made this fail!')
-                        await ctx.send('Well, I failed somehow, send the following to `4JR#2713` (180314310298304512) - resp2: ```py\n' + str(await resp2.json()) + '\n```')
+                        await ctx.send('Well, I failed somehow, send the following to `Pallav#1747` (157355779320446976) - resp2: ```py\n' + str(await resp2.json()) + '\n```')
             else:
-                await ctx.send('Well, I failed somehow, send the following to `4JR#2713` (180314310298304512) - resp: ```py\n' + str(await resp.json()) + '\n```')
+                await ctx.send('Well, I failed somehow, send the following to `Pallav#1747` (157355779320446976) - resp: ```py\n' + str(await resp.json()) + '\n```')
 
 def setup(bot):
     bot.add_cog(Utility(bot))
